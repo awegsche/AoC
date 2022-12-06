4228e51be98e6320de5e7a90e3640d96fc6e4df4
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2022-12-06 18:28:19 +0100

    rangify Day1

--- 2022/day1.h
+++ 2022/day1.h
@@ -15,82 +15,48 @@
 #include <aoc_object.h>
 #include <AocDay.h>
 
-
-using std::cerr, std::cout, std::endl;
-
-class Calories {
-    std::vector<int> m_calories;
-
-public:
-    static auto from_istream(std::istream& stream) -> std::optional<Calories> {
-        std::string line{};
-        Calories cals{};
-        while (std::getline(stream, line)) {
-            if (line.empty()) break;
-
-            try {
-                const int i = std::stoi(line);
-                cals.m_calories.push_back(i);
-            }
-            catch(std::invalid_argument const& ex) {
-                cerr << "invalid arg for int conversion: " << line << endl;
-                return {};
-            }
-            catch(std::out_of_range const& ex) {
-                cerr << "out of range for string " << line << endl;
-                return {};
-            }
-        }
-        return cals;
-    }
-
-    auto sum() const -> int {
-        return std::reduce(m_calories.begin(), m_calories.end());
-    }
-
-    auto empty() const -> bool {
-        return m_calories.empty();
-    }
-};
-
 class Day1: public AocObject<Day1>, public AocDay<Day1, int> {
-    std::vector<Calories> m_elves;
+    std::vector<std::vector<int>> m_elves;
 
 public:
     static constexpr char TITLE[] = "Calorie Counting";
     static constexpr char FILENAME[] = "1";
     static constexpr char YEAR[] = "2022";
 
-
 public:
     static auto get_object(std::istream& stream, Day1& day) -> bool {
+        day.m_elves.clear();
+        day.m_elves.emplace_back();
 
-        do {
-            auto elf = Calories::from_istream(stream);
-            if (!elf)
-                return false;
-            if (elf->empty()) break;
+        std::string line{};
 
-            day.m_elves.push_back(*elf);
-        } while(true);
-
+        while (std::getline(stream, line)) {
+            if (line.empty()) {
+                day.m_elves.emplace_back();
+            }
+            else {
+                day.m_elves.back().emplace_back(std::stoi(line));
+            }
+        }
         return true;
     }
 
     auto part1() const -> value override {
+        using std::views::transform;
+
         return std::ranges::max(
                 m_elves
-                | std::views::transform([](Calories const& c) { return c.sum(); }));
+                | transform([](std::vector<int> const& c) { return std::reduce(c.begin(), c.end()); })
+        );
     }
 
     auto part2() const -> value override {
-        std::vector<int> sum_of_calories;
+        using std::views::transform;
 
-        std::transform(m_elves.cbegin(), m_elves.cend(),
-                       std::back_inserter(sum_of_calories),
-                       [](Calories const& c) { return c.sum(); });
+        auto temp = m_elves | transform([](std::vector<int> const& c) { return std::reduce(c.begin(), c.end()); });
+        std::vector<int> sum_of_calories{temp.begin(), temp.end()};
 
-        std::sort(sum_of_calories.begin(), sum_of_calories.end(), std::greater<int>());
+        std::ranges::sort(sum_of_calories.begin(), sum_of_calories.end(), std::greater<>());
 
         return sum_of_calories[0] + sum_of_calories[1] + sum_of_calories[2];
     }
