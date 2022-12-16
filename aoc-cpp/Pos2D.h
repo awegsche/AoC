ca7e5e190b31f65fd37e76e5c433988ce6362cf1
Andreas Wegscheider <a.wegscheider7141@gmail.com>
2022-12-16 21:08:27 +0100

    clang-format

--- aoc-cpp/Pos2D.h
+++ aoc-cpp/Pos2D.h
@@ -9,31 +9,53 @@
 
 namespace aoc {
 
-    struct Pos2D {
-        int x = 0;
-        int y = 0;
+/// 2-dimensional position vector.
+///
+/// Coordinates are `int`s. This should be enough for all AoC purposes
+struct Pos2D {
+    int x = 0;
+    int y = 0;
 
-        // do your magic
-        auto operator<=>(Pos2D const &) const = default;
+    Pos2D() : x(0), y(0) {}
 
-        inline auto operator-(Pos2D const &other) const -> Pos2D {
-            return Pos2D{x - other.x, y - other.y};
-        }
-        inline auto operator+(Pos2D const &other) const -> Pos2D {
-            return Pos2D{x + other.x, y + other.y};
-        }
+    explicit Pos2D(int a) : x(a), y(a) {}
 
-        inline auto operator+=(Pos2D const &other) {
-            x += other.x;
-            y += other.y;
-        }
-    };
+    Pos2D(int x, int y) : x(x), y(y) {}
 
-}
+    // do your magic
+    // Note: this should create some kind of lexicographical order
+    auto operator<=>(Pos2D const &) const = default;
 
-template<>
-struct std::hash<aoc::Pos2D> {
-    size_t operator()(aoc::Pos2D const& p) const noexcept {
+    [[nodiscard]] inline auto operator-(Pos2D const &other) const -> Pos2D {
+        return Pos2D{x - other.x, y - other.y};
+    }
+
+    [[nodiscard]] inline auto operator+(Pos2D const &other) const -> Pos2D {
+        return Pos2D{x + other.x, y + other.y};
+    }
+
+    inline auto operator+=(Pos2D const &other) -> Pos2D & {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    inline auto operator-=(Pos2D const &other) -> Pos2D & {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    /// returns the manhatten distance `|x| + |y|`
+    [[nodiscard]] inline auto abs_manhatten() const -> int {
+        return std::abs(x) + std::abs(y);
+    }
+};
+
+} // namespace aoc
+
+template <> struct std::hash<aoc::Pos2D> {
+    size_t operator()(aoc::Pos2D const &p) const noexcept {
         return static_cast<int64_t>(p.x) | static_cast<int64_t>(p.y) << 32;
     }
 };
